8c94ca21802484302527c01f6791b1b424a7bf31
Eddy <edgar.vilchis@politicas.unam.mx>
2025-02-11 17:45:28 -0600

    Modificacion en CSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,16 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name'); /* Me parece que aqui se refiere a la clase name del index, le falta un punto*/
-const $b = document.querySelector('#blog');
+
+const $n = document.querySelector('.name'); // En el HTML aparecen atributos de clase los cuales llevan (.)
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
 function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.JSON // Se estaba imprimiendo data, pero no se habia declarado, ademas de que faltaba ponerse con JSON para que no esten los archivos en crudo.
   console.log(data);
-  $n.textContent = '${data.name}';
+  $n.textContent = '${data.name}'; // 
   $b.textContent = '${data.blog}';
   $l.textContent = '${data.location}';
 }
